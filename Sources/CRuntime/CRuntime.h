84e7d858b3845084df9e9c7c83aba2434bda1b3d
Wes Wickwire <wickwirew@gmail.com>
2019-04-06 17:36:01 -0400

    Swift5 (#2)
    * add swift_getTypeByMangledNameInContext
    
    * size_t -> int
    
    * added ;

--- Sources/CRuntime/CRuntime.h
+++ Sources/CRuntime/CRuntime.h
@@ -1,6 +1,9 @@
 #ifndef cruntime_h
 #define cruntime_h
 
-void swift_getFieldAt(const void *base, unsigned index, void (*callback)(const char *name, const void *type, void *ctx), void *callbackCtx);
-
+const void * _Nullable swift_getTypeByMangledNameInContext(
+                        const char *typeNameStart,
+                        int typeNameLength,
+                        const void *context,
+                        const void * const *genericArgs); 
 #endif
